1b2b72c13b7f86ed5bf378e1be7f232c17ffb346
Kyle DeFreitas <kyle.e.defreitas@gmail.com>
2013-04-29 20:13:07 -0400

    init

---
+++ theme.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Initializr_BootStrap_Responsive extends Theme
+{
+    public $name			= 'Initializr BootStrap Responsive';
+    public $author			= 'Jonathan Verrecchia (adapted by Kyle E DeFreitas)';
+    public $author_website	= 'http://verekia.com/';
+    public $website			= 'http://www.initializr.com/';
+    public $description		= 'A HTML5-CSS3 template to kickstart your design. Fully compatible with every device and browser.';
+    public $version			= '2.3.0';
+}
